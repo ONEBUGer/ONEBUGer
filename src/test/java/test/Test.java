3be77ca429b668ce7ac30f90839b3bee6001eedf
zhengchangbing <329971727@qq.com>
2022-08-15 14:05:10 +0800

    新增测试文件

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @author ZhengChangBing
+ * @Date 2022/8/15 14:01
+ * @Description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test class");
+    }
+}
